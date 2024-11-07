ec169f2a8b028d0c74e476c4c514a0d8323a5a47
Azulina134 <rosariomendozaharenazul@gmail.com>
2024-11-07 02:36:33 -0700

    Update group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('🍭 Chat Baneado con exito.')
+    m.reply('𝐒𝐮𝐛-𝐁𝐨𝐭 𝐃𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨🚩.')
 }
-handler.help = ['banearbot']
-handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
+handler.help = ['Bot Off']
+handler.tags = ['Serbot']
+handler.command = ['Bot off', 'Bot off']
 handler.group = true 
 export default handler
