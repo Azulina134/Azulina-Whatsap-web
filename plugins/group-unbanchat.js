fb0e5d23ba6c9c2547520847380f68523d1c363f
Azulina134 <rosariomendozaharenazul@gmail.com>
2024-11-07 02:38:58 -0700

    Update group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('🍭 Bot activo en este grupo.')   
+    m.reply('𝐒𝐮𝐛-𝐁𝐨𝐭 𝐀𝐜𝐭𝐢𝐯𝐚𝐝𝐨🚩.')   
 }
-handler.help = ['desbanearbot']
-handler.tags = ['group']
-handler.command = ['desbanearbot', 'unbanchat']
+handler.help = ['Bot off']
+handler.tags = ['serbot']
+handler.command = ['Bot on', 'Bot on']
 handler.group = true 
 export default handler
