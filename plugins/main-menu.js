859dcf3d49e32418734f2062112a2a1bc8ce4fe6
Azulina134 <rosariomendozaharenazul@gmail.com>
2024-11-07 10:16:36 -0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -41,9 +41,9 @@ const defaultMenu = {
 
 \t\t\t*L I S T A  -  M E N Ú S*
 `.trimStart(),
-header: '╭──⬣「 *%category* 」⬣',
+header: '╭✶⊶⊷⊶⊷❍「 *%category* 」❍',
 body: '│  ≡◦ *%cmd*\n',
-footer: '╰──⬣\n',
+footer: '╰✶⊶⊷⊶⊷⊶⊷⊶⊷✶⊶⊷⊶⊷⊶⊷✶⊶⊷❍\n',
 after: '',
 }
 
